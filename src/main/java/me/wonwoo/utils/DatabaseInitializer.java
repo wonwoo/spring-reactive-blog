948143e11cd9079a307b55c7209a2b9192e4f57f
wonwoo <aoruqjfu@gmail.com>
2017-11-13 10:48:37 +0900

    datainit

--- src/main/java/me/wonwoo/utils/DatabaseInitializer.java
+++ src/main/java/me/wonwoo/utils/DatabaseInitializer.java
@@ -1,14 +1,15 @@
 package me.wonwoo.utils;
 
+import java.time.LocalDateTime;
+import java.util.Arrays;
+
+import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.stereotype.Component;
+
 import me.wonwoo.domain.Post;
 import me.wonwoo.domain.PostRepository;
 import me.wonwoo.domain.User;
 import me.wonwoo.domain.UserRepository;
-import org.springframework.beans.factory.SmartInitializingSingleton;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-import java.util.Arrays;
 
 @Component
 public class DatabaseInitializer implements SmartInitializingSingleton {
@@ -24,43 +25,42 @@ public class DatabaseInitializer implements SmartInitializingSingleton {
   @Override
   public void afterSingletonsInstantiated() {
 
+      User user1 = new User("wonwoo");
+      User user2 = new User("kevin");
 
-    userRepository.deleteAll().block();
-    postRepository.deleteAll().block();
+      Post post1 = new Post(
+              "Spring5 reactive",
+              "With the release of [Spring Framework 5.0](https://spring.io/blog/2017/09/28/spring-framework-5-0-goes-ga) now just happening, you can imagine this is a giant step for Project Reactor :)\n",
+              user1,
+              LocalDateTime.of(2017, 9, 28, 12, 0)
+      );
+      Post post2 = new Post(
+              "Spring5 reactive source",
+              "" +
+                      "```java\n" +
+                      "@SpringBootApplication\n" +
+                      "public class SpringReactiveBlogApplication {\n" +
+                      "\n" +
+                      "\tpublic static void main(String[] args) {\n" +
+                      "\t\tSpringApplication.run(SpringReactiveBlogApplication.class, args);\n" +
+                      "\t}\n" +
+                      "}\n```\n",
+              user2,
+              LocalDateTime.of(2017, 11, 20, 11, 10)
+      );
+      Post post3 = new Post(
+              "Spring reactor",
+              "### Flux \n A Reactive Streams Publisher with rx operators that emits `0` to `N` elements, and then completes (successfully or with an error). \n ### Mono  \n A Reactive Streams Publisher with basic rx operators that completes successfully by emitting an element, or with an error.\n",
+              user1,
+              LocalDateTime.of(2017, 12, 25, 07, 16)
+      );
 
-    User user1 = new User("wonwoo");
-    User user2 = new User("kevin");
-    this.userRepository.saveAll(Arrays.asList(user1, user2))
-        .subscribe(System.out::println);;
+      userRepository.deleteAll()
+            .thenMany(postRepository.deleteAll())
+            .thenMany(this.userRepository.saveAll(Arrays.asList(user1, user2)))
+            .thenMany(postRepository.saveAll(Arrays.asList(post1, post2, post3)))
+            .subscribe(null, null, () -> postRepository.findAll()
+                    .subscribe(System.out::println));
 
-    Post post1 = new Post(
-        "Spring5 reactive",
-        "With the release of [Spring Framework 5.0](https://spring.io/blog/2017/09/28/spring-framework-5-0-goes-ga) now just happening, you can imagine this is a giant step for Project Reactor :)\n",
-        user1,
-        LocalDateTime.of(2017, 9, 28, 12, 0)
-    );
-    Post post2 = new Post(
-        "Spring5 reactive source",
-        "" +
-            "```java\n" +
-            "@SpringBootApplication\n" +
-            "public class SpringReactiveBlogApplication {\n" +
-            "\n" +
-            "\tpublic static void main(String[] args) {\n" +
-            "\t\tSpringApplication.run(SpringReactiveBlogApplication.class, args);\n" +
-            "\t}\n" +
-            "}\n```\n",
-        user2,
-        LocalDateTime.of(2017, 11, 20, 11, 10)
-    );
-    Post post3 = new Post(
-        "Spring reactor",
-        "### Flux \n A Reactive Streams Publisher with rx operators that emits `0` to `N` elements, and then completes (successfully or with an error). \n ### Mono  \n A Reactive Streams Publisher with basic rx operators that completes successfully by emitting an element, or with an error.\n",
-        user1,
-        LocalDateTime.of(2017, 12, 25, 07, 16)
-    );
-
-    postRepository.saveAll(Arrays.asList(post1, post2, post3))
-        .subscribe(System.out::println);
   }
 }
