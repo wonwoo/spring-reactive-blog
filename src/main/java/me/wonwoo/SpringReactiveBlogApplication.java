5945d7daf80933931cbe0257e2b9eb2fcc0d0382
wonwoo <aoruqjfu@gmail.com>
2017-11-26 20:14:46 +0900

    mustache

--- src/main/java/me/wonwoo/SpringReactiveBlogApplication.java
+++ src/main/java/me/wonwoo/SpringReactiveBlogApplication.java
@@ -1,10 +1,19 @@
 package me.wonwoo;
 
+import com.samskivert.mustache.Mustache;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.samskivert.mustache.Mustache.compiler;
 
 @SpringBootApplication
 public class SpringReactiveBlogApplication {
+
+	@Bean
+	public Mustache.Compiler mustacheCompiler(Mustache.TemplateLoader loader) {
+		return compiler().escapeHTML(false).withLoader(loader);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringReactiveBlogApplication.class, args);
