0fda8b4da95f8c3a545a8ae032adfd698b28fa52
wonwoo <aoruqjfu@gmail.com>
2019-08-12 22:08:55 +0900

    add test

--- src/test/kotlin/ml/wonwoo/utils/ExtensionsKtTests.kt
+++ src/test/kotlin/ml/wonwoo/utils/ExtensionsKtTests.kt
@@ -7,12 +7,58 @@ import java.time.LocalDateTime
 internal class ExtensionsKtTests{
 
     @Test
-    fun `format to english test`() {
+    fun `format to english th test`() {
+
         val date = LocalDateTime.of(2019, 8, 12, 12,22)
 
         val formatToEnglish = date.formatToEnglish()
 
         assertThat(formatToEnglish).isEqualTo("August 12th 2019")
+
+    }
+
+    @Test
+    fun `format to english th2 test`() {
+
+        val date = LocalDateTime.of(2019, 8, 4, 12,22)
+
+        val formatToEnglish = date.formatToEnglish()
+
+        assertThat(formatToEnglish).isEqualTo("August 4th 2019")
+
+    }
+
+    @Test
+    fun `format to english nd test`() {
+
+        val date = LocalDateTime.of(2019, 8, 22, 12,22)
+
+        val formatToEnglish = date.formatToEnglish()
+
+        assertThat(formatToEnglish).isEqualTo("August 22nd 2019")
+
+    }
+
+    @Test
+    fun `format to english st test`() {
+
+        val date = LocalDateTime.of(2019, 8, 21, 12,22)
+
+        val formatToEnglish = date.formatToEnglish()
+
+        assertThat(formatToEnglish).isEqualTo("August 21st 2019")
+
+    }
+
+
+    @Test
+    fun `format to english rd test`() {
+
+        val date = LocalDateTime.of(2019, 8, 23, 12,22)
+
+        val formatToEnglish = date.formatToEnglish()
+
+        assertThat(formatToEnglish).isEqualTo("August 23rd 2019")
 
     }
 }
