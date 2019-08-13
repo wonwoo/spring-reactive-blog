94f8932d9404c26e2bf6651faa13b09870c53b03
wonwoo <aoruqjfu@gmail.com>
2019-08-13 21:12:02 +0900

    Polish

--- src/main/kotlin/ml/wonwoo/utils/MarkDownConverter.kt
+++ src/main/kotlin/ml/wonwoo/utils/MarkDownConverter.kt
@@ -4,7 +4,6 @@ import org.commonmark.ext.autolink.AutolinkExtension
 import org.commonmark.parser.Parser
 import org.commonmark.renderer.html.HtmlRenderer
 import org.springframework.stereotype.Service
-import org.springframework.util.StringUtils
 
 @Service
 class MarkDownConverter : (String?) -> String {
@@ -15,13 +14,11 @@ class MarkDownConverter : (String?) -> String {
 
     override fun invoke(input: String?): String {
 
-        if (!StringUtils.hasText(input)) {
+        return input?.let {
 
-            return ""
+            this.renderer.render(this.parser.parse(it))
 
-        }
-
-        return this.renderer.render(this.parser.parse(input))
+        } ?: ""
 
     }
 
