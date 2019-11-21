de8ddc227349263f070f36730e143b95e4e4340e
wonwoo <aoruqjfu@gmail.com>
2019-11-21 21:24:20 +0900

    version upgrade

--- src/main/kotlin/ml/wonwoo/SpringReactiveBlogApplication.kt
+++ src/main/kotlin/ml/wonwoo/SpringReactiveBlogApplication.kt
@@ -1,11 +1,12 @@
 package ml.wonwoo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 class SpringReactiveBlogApplication
-
 
 fun main(args: Array<String>) {
     runApplication<SpringReactiveBlogApplication>(*args)
--- src/main/kotlin/ml/wonwoo/client/BlogProperties.kt
+++ src/main/kotlin/ml/wonwoo/client/BlogProperties.kt
@@ -1,8 +1,10 @@
 package ml.wonwoo.client
 
 import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConfigurationProperties("blog")
+@ConstructorBinding
 class BlogProperties(
 
     val url: String
